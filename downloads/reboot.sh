5a538158bfeb88146e03195e124e432ab0d8195c
Theo <144005847+TheoRichter@users.noreply.github.com>
2024-12-02 10:20:41 +0100

    Create reboot.sh

---
+++ downloads/reboot.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+############################################################
+#                                                          #
+#          Monatlicher Reboot des Systems                  #
+#                                                          #
+############################################################
+
+#   Ausgabe der Status Meldung #
+echo
+echo "Start des monatlicher Reboot des Systems"
+echo
+systemctl reboot
+#   Ausgabe der Status Meldung #
+echo
+echo "Ende des monatlichen Reboots des Systems"
+echo
